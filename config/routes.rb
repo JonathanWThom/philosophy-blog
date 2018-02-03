614482a23883b86ba8c3176fe212c6c5ff73f632
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-02-02 23:32:26 -0800

    Some authentication goodness

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
+
   resources :posts
+  get "/dashboard", to: "dashboard#index"
 end
