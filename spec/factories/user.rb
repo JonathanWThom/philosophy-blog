26e0975718d417c020cb1edb5503cec8c019fba6
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-02-02 22:49:07 -0800

    Remove some devise routes and slimify layouts

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Seinfeld.unique.character }
     email { Faker::Internet.unique.email }
     password "12345678"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,5 @@
 describe User do
   it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+  it { should validate_presence_of :name }
 end
