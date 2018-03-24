bd685f7f40b807f606f5f563f0ff31ae0e5d7970
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-03-24 12:19:37 -0700

    Post controller specs

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,104 @@
+describe PostsController, type: :request do
+  context "when a user is not authenticated" do
+    describe "#index" do
+      it "allows users to see posts" do
+        get posts_path
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "#show" do
+      it "allows users to see an individual post" do
+        post = create(:post)
+        get post_path(post)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "#new" do
+      it "redirects unauthenticated users" do
+        get new_post_path
+        expect(response.status).to eq(302)
+      end
+    end
+
+    describe "#create" do
+      it "redirects unauthenticated users" do
+        post posts_path
+        expect(response.status).to eq(302)
+      end
+    end
+
+    describe "#edit" do
+      it "redirects unauthenticated users" do
+        post = create(:post)
+        get edit_post_path(post)
+        expect(response.status).to eq(302)
+      end
+    end
+
+    describe "#update" do
+      it "redirects unauthenticated users" do
+        post = create(:post)
+        patch post_path(post)
+        expect(response.status).to eq(302)
+      end
+    end
+
+    describe "#destroy" do
+      it "redirects unauthenticated users" do
+        post = create(:post)
+        delete post_path(post)
+        expect(response.status).to eq(302)
+      end
+    end
+  end
+
+  context "when user is authenticated" do
+    before(:each) do
+      user = create(:user)
+      login_as(user)
+    end
+
+    describe "#new" do
+      it "succeeds" do
+        get new_post_path
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "#create" do
+      it "creates a new post" do
+        attributes = build(:post).attributes
+        post posts_path, params: { post: attributes }
+        expect(Post.count).to eq(1)
+      end
+    end
+
+    describe "#edit" do
+      it "succeeds" do
+        post = create(:post)
+        get edit_post_path(post)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "#update" do
+      it "will update a post" do
+        post = create(:post)
+        new_title = Faker::Beer.name
+        patch post_path(post), params: { post: { title: new_title } }
+        post = Post.find_by_id(post.id)
+        expect(post.title).to eq(new_title)
+      end
+    end
+
+    describe "#destroy" do
+      it "destroys a post" do
+        post = create(:post)
+        delete post_path(post)
+        expect(Post.count).to eq(0)
+      end
+    end
+  end
+end
--- spec/modules/sluggable_spec.rb
+++ spec/modules/sluggable_spec.rb
@@ -1,21 +1,24 @@
 describe Sluggable do
+  attr_reader :post
+
+  before(:each) do
+    @post = create(:post)
+  end
+
   describe "#to_param" do
     it "returns the slug" do
-      post = create(:post)
       expect(post.to_param).to eq(post.slug)
     end
   end
 
   describe "#create_slug" do
     it "sets the slug to the parameterized title" do
-      post = create(:post)
       expect(post.slug).to eq(post.title.parameterize)
     end
   end
 
   describe ".find" do
     it "find the record by the slug" do
-      post = create(:post)
       expect(Post.find(post.slug)).to eq(post)
     end
   end
